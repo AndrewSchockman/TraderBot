8ac587c739f3749b61de1bb313c97fd48d4009c1
AndrewSchockman <schockman.6@gmail.com>
2025-09-30 21:07:34 -0400

    Edited sp500 data
    Established a target value to predict, cut out all stock data prior to 1/1/2000

--- TraderBot.py
+++ TraderBot.py
@@ -3,5 +3,10 @@ import matplotlib.pyplot as plt
 
 sp500 = yf.Ticker("^GSPC")
 sp500 = sp500.history(period="max")
-sp500.plot.line(y="Close", use_index=True)
-plt.show()
+del sp500["Dividends"]
+del sp500["Stock Splits"]
+sp500["Tomorrow"] = sp500["Close"].shift(-1)
+sp500["Target"] = (sp500["Tomorrow"] > sp500["Close"]).astype(int)
+sp500 = sp500.loc["2000-1-1":].copy()
+
+print(sp500)
